50436a801aeeefab0b3e8075893b3ce94cc55db8
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-26 02:26:16 -0800

    initial commit
    Create initial Project structure and outline.

---
+++ src/main/java/DoubleIt.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DoubleIt {
+    public static void main(String[] args) {
+        System.out.println("Give me a number, and I will double it for you!");
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            Integer yourNumber = bufferedReader.readLine();
+            Integer yourDoubleNumber = yourNumber * 2;
+
+            System.out.println("I doubled your number fo you: " + yourDoubleNumber);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
